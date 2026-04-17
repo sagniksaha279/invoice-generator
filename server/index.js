3812c62ad56f53ffd368fbf1f3ae2cc26737911a
Sagnik <sahasagnik279@gmail.com>
2026-04-18 02:59:56 +0530

    puppeteer fixation

--- server/index.js
+++ server/index.js
@@ -458,7 +458,13 @@ app.post("/api/generate-pdf", authMiddleware, async (req, res) => {
     const html = buildInvoiceHTML(invoice);
     const browser = await puppeteer.launch({
       headless: true,
-      args: ["--no-sandbox","--disable-setuid-sandbox","--disable-dev-shm-usage","--disable-gpu"]
+      executablePath: puppeteer.executablePath(),
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-gpu"
+      ]
     });
     const page = await browser.newPage();
 
